52c8479180eb6f3b0edfe82c31e19650cf34b84e
Megan Smith <megans766@gmail.com>
2023-02-23 11:43:32 -0500

    create connects route

---
+++ routes/connects.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const votesCtrl = require('../controllers/votes.js')
+const middleware = require('../middleware/auth.js')
+
+const { decodeUserFromToken, checkAuth } = middleware
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+
+
+module.exports = router
+
