a21529af51616d5facf9c99b7f3f620264f74fc4
Megan Smith <megans766@gmail.com>
2023-02-23 12:57:01 -0500

    update connects controller with create application functionality

--- controllers/connects.js
+++ controllers/connects.js
@@ -1,5 +1,14 @@
 const { Connect } = require('../models')
 
+async function create(req, res) {
+  try {
+    const application = await Connect.create(req.body)
+    res.status(200).json(application)
+  } catch (error) {
+    res.status(500).json({ err: error })
+  }
+}
+
 module.exports = {
-  
+  create
 }
--- routes/connects.js
+++ routes/connects.js
@@ -10,6 +10,7 @@ const { decodeUserFromToken, checkAuth } = middleware
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 
+router.post('/', checkAuth, connectsCtrl.create)
 
 module.exports = router
 
